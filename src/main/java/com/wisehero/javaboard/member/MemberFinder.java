7b077720ad79484c672442c6fcaa6a87e76e45d6
wisehero <godjw96@gmail.com>
2025-11-26 16:35:06 +0900

    MemberService -> MemberFinder로 변경

--- src/main/java/com/wisehero/javaboard/member/MemberFinder.java
+++ src/main/java/com/wisehero/javaboard/member/MemberFinder.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberFinder {
 
     private final MemberJpaRepository memberRepository;
 
