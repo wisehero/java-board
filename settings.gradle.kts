69d8d0d0edae0d72e0bdccdf6dad25d346c18c33
wisehero <godjw96@gmail.com>
2025-12-08 14:14:54 +0900

    java-board(chore) : 파일 경로 정리 및 Gradle 설정 개선
    * 여러 파일의 경로를 정리하고, Gradle 설정을 개선하여 프로젝트 구조를 명확히 하였습니다.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,28 @@
 rootProject.name = "java-board"
+
+include(
+    ":apps:board-api",
+    ":supports:jackson",
+    ":supports:logging",
+    ":supports:monitoring"
+)
+
+pluginManagement {
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+
+    repositories {
+        maven { url = uri("https://repo.spring.io/milestone") }
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+            }
+        }
+    }
+}
